09c2d21d5ab351f6ca5ac537163e2d8fa6f69944
ani0813 <ani.guloyan.13@gmail.com>
2023-03-09 13:34:05 +0400

    added prev thing

--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,35 @@
+let prev = null;
 // Listen for mouseup events
 document.addEventListener("mouseup", function(event) {
     // Get the selected text
-    var selectedText = window.getSelection().toString().trim();
+    let selectedText = window.getSelection().toString().trim();
     
     // If text is selected, highlight it and display a message above it
     if (selectedText !== "") {
-      // Create a span element with the selected text
-      var highlightedText = document.createElement("span");
-      highlightedText.textContent = selectedText;
-      highlightedText.style.backgroundColor = "yellow";
-      
+      if (prev !== null) {
+        prev.remove();
+      }
       // Create a div element for the message
-      var message = document.createElement("div");
+      let message = document.createElement("div");
       message.textContent = "Emotion";
-      message.style.backgroundColor = "gray";
+      message.style.backgroundColor = "yellow";
+      message.style.fontWeight = "bold";
       message.style.color = "white";
       message.style.padding = "5px";
       message.style.position = "absolute";
-      message.style.top = event.pageY - 30 + "px";
+      message.style.top = event.pageY - 50 + "px";
       message.style.left = event.pageX + "px";
       
       // Add the highlighted text and message to the page
-      document.body.appendChild(highlightedText);
       document.body.appendChild(message);
+      prev = message;
+    }
+    else if (prev !== null) {
+      prev.remove();
+      prev = null;
     }
 });
-  
+
+
+
+
